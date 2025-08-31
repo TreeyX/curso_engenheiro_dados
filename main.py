b9d81f700d6fef63ca0c8e18dc27e92a3096979f
Vagner Michell <vagner.michell@gmail.com>
2025-08-31 07:46:11 -0300

    fix: removido apelido do select top1

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ with open('./config.yaml', 'r', encoding='utf-8') as arquivo_configuracao:
     config = yaml.safe_load(arquivo_configuracao)
 
 postgres_config = config['credenciais_postgres']
+caminho_final = config['caminho_etl_final']
 
 # %%
 connection_string = f"postgresql+psycopg2://{postgres_config['user']}:{postgres_config['password']}@{postgres_config['host']}:{postgres_config['port']}/{postgres_config['database']}"
@@ -34,13 +35,13 @@ for arquivos in lista_arquivos_raw:
     arquivo = os.path.basename(arquivos)
     nome_tabela = arquivo.split(".csv")[0]
     with engine.begin() as conn:
-        resultado = conn.execute(text(f"select 1 from {nome_tabela} od limit 1")).fetchone()
+        resultado = conn.execute(text(f"select 1 from {nome_tabela} limit 1")).fetchone()
 
         if resultado is None:
-            df = pd.read_csv(fr".\data\raw_data\{nome_tabela}.csv", sep=';', encoding='utf-8')
+            df = pd.read_csv(fr".\{caminho_arquivos_raw}\{nome_tabela}.csv", sep=';', encoding='utf-8')
             df.to_sql(nome_tabela, conn, if_exists='append', index=False)
         else:
-            print('Sem tabelas novas!', end='\r')
+            print(f"Tabela {nome_tabela} já existe!", end='\r')
 
 # %%
 with open('./queries/atividade_1.sql', 'r', encoding='utf8') as disconto:
@@ -48,6 +49,6 @@ with open('./queries/atividade_1.sql', 'r', encoding='utf8') as disconto:
 
 # %%
 df_vendas_desconto = pd.read_sql(query, engine)
-df_vendas_desconto.to_parquet(fr'.\data\etl_data\vendas_desconto.parquet', engine='fastparquet', compression='snappy')
+df_vendas_desconto.to_parquet(fr'.{caminho_final}\vendas_desconto.parquet', engine='fastparquet', compression='snappy')
 
 
