5ef44eedb729a7f14da2e665677515179d1eb6a8
Vagner Michell <vagner.michell@gmail.com>
2025-08-31 08:37:44 -0300

    fix: verificar se tabela existe não vai dar mais erro

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 import yaml
 import os
 from pathlib import Path
-from sqlalchemy import create_engine, text, inspect
+from sqlalchemy import create_engine, text
+from sqlalchemy.exc import ProgrammingError
 
 # %%
 with open('./config.yaml', 'r', encoding='utf-8') as arquivo_configuracao:
@@ -15,8 +16,6 @@ caminho_final = config['caminho_etl_final']
 # %%
 connection_string = f"postgresql+psycopg2://{postgres_config['user']}:{postgres_config['password']}@{postgres_config['host']}:{postgres_config['port']}/{postgres_config['database']}"
 engine = create_engine(connection_string)
-raw_engine = engine.raw_connection()
-insp = inspect(engine)
 
 # %%
 with open('./ddl/create_table.sql', 'r', encoding='utf8') as criar_tabela:
@@ -34,14 +33,21 @@ lista_arquivos_raw = list(caminho_arquivos_raw.glob("*.csv"))
 for arquivos in lista_arquivos_raw:
     arquivo = os.path.basename(arquivos)
     nome_tabela = arquivo.split(".csv")[0]
-    with engine.begin() as conn:
-        resultado = conn.execute(text(f"select 1 from {nome_tabela} limit 1")).fetchone()
+    arquivo_csv = caminho_arquivos_raw / f"{nome_tabela}.csv"
 
-        if resultado is None:
-            df = pd.read_csv(fr".\{caminho_arquivos_raw}\{nome_tabela}.csv", sep=';', encoding='utf-8')
+    with engine.begin() as conn:
+        try:
+            resultado = conn.execute(text(f"select 1 from {nome_tabela} limit 1")).fetchone()
+            tabela_existe = True
+        except ProgrammingError:
+            tabela_existe = False
+
+        if not tabela_existe:
+            df = pd.read_csv(arquivo_csv, sep=';', encoding='utf-8')
             df.to_sql(nome_tabela, conn, if_exists='append', index=False)
+            print(f"Tabela {nome_tabela} não existe, criando!", end='\r')
         else:
-            print(f"Tabela {nome_tabela} já existe!", end='\r')
+            print(f"Tabela {nome_tabela} já existe!")
 
 # %%
 with open('./queries/atividade_1.sql', 'r', encoding='utf8') as disconto:
